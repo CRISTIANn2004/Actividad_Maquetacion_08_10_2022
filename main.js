986b219a893890a9181fd977b31fff6a474c62eb
CristianAndresSilvaMuñozADSO <socarraass@gmail.com>
2022-10-21 17:42:38 -0500

    main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-addEventListener
+addEventListener("DOMContentLoaded", (e)=>{
+    let imgSlider = (anything)=>{
+        document.querySelector(".starbucks").src = anything; 
+    }
+    document.querySelector(".thumb").addEventListener("click", (e)=>{
+        if(e.target.nodeName != "UL"){
+            imgSlider(e.target.dataset.img)
+        }
+    }) 
+})
